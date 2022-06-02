7d3531cc7892bd2c4d3315b1a552992c2f6ea550
BartoszRadtke <bartosz.radtke2001@gmail.com>
2022-06-02 20:26:32 +0200

    added repetiton code

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import numpy as np
 import CRC32_code as crc
 import parity_check
+import repetition_code
 from numpy import random
 import math
 import komm
 
 resending_counter = 0
 crc_code = crc.CyclicRedundancyCheck()
+repetition = repetition_code.RepetitionCode()
 
 # poniższe parametry zmieniamy, aby wyszły rózne wyniki
-data_size = 800
-packets_size = 4
-error_probability = 0.05
+data_size = 10000
+packets_size = 100
+error_probability = 0.001
 
 
 def restart_counters():
@@ -25,11 +27,13 @@ def split_into_packets(array: np.ndarray) -> np.ndarray:
 
 
 def test(packets: np.ndarray, code_type):
-    if code_type == 1 or code_type == 2:
+    if code_type == 1 or code_type == 2 or code_type == 3:
         if code_type == 1:
             print("CRC32: ")
         if code_type == 2:
             print("Parity: ")
+        if code_type == 3:
+            print("Repetition: ")
         global resending_counter
         errors_detected = 0
         errors_not_detected = 0
@@ -43,7 +47,11 @@ def test(packets: np.ndarray, code_type):
                 encoded_packet = crc_code.encode(packet)
             if code_type == 2:
                 encoded_packet = parity_check.parity_check_encode(packet)
+            if code_type == 3:
+                encoded_packet = repetition.encode(packet)
+
             packet_after_noise = bsc(encoded_packet)
+
 
             if code_type == 1:
                 if crc_code.check(packet_after_noise):
@@ -65,6 +73,17 @@ def test(packets: np.ndarray, code_type):
                     errors_detected += 1
                     array.append(packet)
 
+            if code_type == 3:
+                if repetition.check(packet_after_noise):
+                    if np.array_equal(encoded_packet, packet_after_noise):
+                        unchanged_packets += 1
+                    else:
+                        errors_not_detected += 1
+                else:
+                    errors_detected += 1
+                    if repetition.should_repeat():
+                        array.append(packet)
+
         print("Pomyslnie przeslane pakiety", unchanged_packets)
         print("Wykryte bledy", errors_detected)
         print("Niewykryte bledy", errors_not_detected)
@@ -77,13 +96,20 @@ def test(packets: np.ndarray, code_type):
             test(np.array(array), code_type)
 
 
+def clear_console():
+    print()
+    print("---------------------------------------------------")
+    restart_counters()
+
+
 if __name__ == '__main__':
     test_array = random.randint(2, size=data_size)
     test_packets = split_into_packets(test_array)
     test(test_packets, 1)
     print("Ile razy ponownie przesyłano pakiety CRC: ", resending_counter)
-    print()
-    print("---------------------------------------------------")
-    restart_counters()
+    clear_console()
     test(test_packets, 2)
     print("Ile razy ponownie przesyłano pakiety Parzystość: ", resending_counter)
+    clear_console()
+    test(test_packets, 3)
+    print("Ile razy ponownie przesyłano pakiety kod dublowania: ", resending_counter)
---
+++ repetition_code.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+class RepetitionCode:
+    def __init__(self):
+        self.__repetition = 3
+        self.__shouldRepeat = False
+        self.__packet_length = 0
+
+    def encode(self, array: np.ndarray) -> np.ndarray:
+        encoded = []
+        self.__packet_length = array.size
+
+        for i in range(self.__repetition):
+            encoded = np.append(encoded, array)
+
+        return encoded
+
+    def check(self, array: np.ndarray) -> bool:
+        array = self.separate_packets(array)
+        errors = 0
+        var = array[0]
+        for arr in array:
+            if not np.array_equal(var, arr):
+                errors += 1
+
+        if (len(array) - 1) == errors:
+            self.__shouldRepeat = True
+        else:
+            self.__shouldRepeat = False
+        if errors >= 1:
+            return False
+        return True
+
+    def separate_packets(self, array: np.ndarray) -> np.ndarray:
+        return array.reshape(self.__repetition, self.__packet_length)
+
+    def should_repeat(self):
+        return self.__shouldRepeat
+
+    def get_packet_length(self):
+        return self.__packet_length
