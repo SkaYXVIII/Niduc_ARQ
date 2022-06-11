df93a9df3d0963ca48627f384378b1510c11d609
BartoszRadtke <bartosz.radtke2001@gmail.com>
2022-06-11 16:30:28 +0200

    clean_up code for test reason

--- repetition_code.py
+++ repetition_code.py
@@ -4,7 +4,6 @@ import numpy as np
 class RepetitionCode:
     def __init__(self):
         self.__repetition = 3
-        self.__shouldRepeat = False
         self.__packet_length = 0
 
     def encode(self, array: np.ndarray) -> np.ndarray:
@@ -25,20 +24,12 @@ class RepetitionCode:
             if not np.array_equal(var, arr):
                 errors += 1
 
-        if (len(array) - 1) == errors and not np.array_equal(array[1], array[2]):
-            self.__shouldRepeat = True
-        else:
-            self.__shouldRepeat = False
-
         if errors >= 1:
             return False
         return True
 
     def separate_packets(self, array: np.ndarray) -> np.ndarray:
         return array.reshape(self.__repetition, self.__packet_length)
-
-    def should_repeat(self):
-        return self.__shouldRepeat
 
     def get_packet_length(self):
         return self.__packet_length
